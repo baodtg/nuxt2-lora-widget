cfc7dc90e8c3fc321393053fbefb395899165742
baodtg <baodtg@belive.sg>
2024-08-06 10:58:42 +0700

    remove console.log

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,6 @@
 <script>
 export default {
   async asyncData(context) {
-    console.log(context)
     const htmlContent = await fetch('http://localhost:3000/script.txt').then((res) => res.text());
     return {
       htmlContent,
