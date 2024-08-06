e9548026fde9fca005d4828b6df875e86dfc7fa6
baodtg <65148548+baodtg@users.noreply.github.com>
2024-08-06 10:29:35 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-html="htmlContent"></div>
+</template>
+
+<script>
+export default {
+  async asyncData() {
+    const htmlContent = await fetch(
+      'https://githubrhmnjf-adkb--3000--70dbe416.local-credentialless.webcontainer.io/script.txt'
+    ).then((res) => res.text());
+    return {
+      htmlContent,
+    };
+  },
+  data() {
+    return {
+      htmlContent: '',
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
